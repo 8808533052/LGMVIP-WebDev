a0823999dc78e20d8928dadfa30d2f25c235fc45
Mohammad Abdullah <72933566+8808533052@users.noreply.github.com>
2021-07-28 16:21:45 +0530

    Add files via upload

---
+++ task2/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Users from './Users'
+
+const Home = () => {
+    return (
+        <div className="home">
+                click on button in the Navbar saying " Get Users" to fetch 
+            </div>
+    )
+}
+
+export default Home
